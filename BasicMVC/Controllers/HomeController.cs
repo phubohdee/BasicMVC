c7ff0e3722cd2cac05788180898148063aa84946
Phubohdee <Phubohdee@DESKTOP-E19J6FM>
2023-02-28 15:52:34 +0700

    Add project files.

---
+++ BasicMVC/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using BasicMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BasicMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            //Member m1 = new Member();
+            //m1.ID = 100;
+            //m1.FullName = "Phubohdee Areesawangkit";
+            //m1.Address = "Thailand";
+
+            //Member m2 = new Member();
+            //m2.ID = 200;
+            //m2.FullName = "Bill";
+            //m2.Address = "BKK";
+
+            //Member m3 = new Member();
+            //m3.ID = 300;
+            //m3.FullName = "Bill Aree";
+            //m3.Address = "Nonthaburi";
+
+            //List<Member> MemberLists = new List<Member>();
+            //MemberLists.Add(m1);
+            //MemberLists.Add(m2);
+            //MemberLists.Add(m2);
+
+            //return View(MemberLists);
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(Member model)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Result", model);
+            }
+
+            return View(model);
+        }
+
+        public IActionResult Faq()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ BasicMVC/Models/Member.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BasicMVC.Models
+{
+    public class Member
+    {
+        //public int ID { get; set; }
+        [Required(ErrorMessage = "กรุณาป้อนชื่อ-สกุลด้วย")]
+        public string FullName { get; set; }
+        [Required(ErrorMessage = "กรุณาป้อนที่อยู่ด้วย")]
+        public string Address { get; set; }
+    }
+}
